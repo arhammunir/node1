6cf810ee46baaa430be89111175879f76e34e7b7
unknown <nodejswebproject1@gmail.com>
2021-02-25 05:38:34 -0800

    committed

--- app.js
+++ app.js
@@ -238,7 +238,6 @@ app.post("/uploadfile", upload_video , (req, res)=>{
 		video: req.file.filename
 	})
 	var id= video_stream_data._id;
-	res.render("detail", {id : id});
 	var data = await video_stream_data.save();
 	res.redirect("/details/"+id);
  	}catch{
