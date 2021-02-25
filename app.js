0320acc60082c2f6d754aee5f6245deeb71db1bc
unknown <nodejswebproject1@gmail.com>
2021-02-25 05:48:10 -0800

    committed

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ try{
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 }catch{
 	(e)=>{console.log(`READ ERROR IS ${e}`)}
 }
